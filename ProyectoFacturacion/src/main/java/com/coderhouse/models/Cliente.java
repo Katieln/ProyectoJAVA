b1ad7506b6eed8d342419c4ee792398c917de81f
Katieln <139283838+Katieln@users.noreply.github.com>
2024-11-01 06:50:12 -0500

    alter tables

--- ProyectoFacturacion/src/main/java/com/coderhouse/models/Cliente.java
+++ ProyectoFacturacion/src/main/java/com/coderhouse/models/Cliente.java
@@ -1,5 +1,6 @@
 package com.coderhouse.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,5 +13,6 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // Identificador único del cliente
 
-    private String nombre; // Nombre del cliente
+    @Column (name = "nombreCliente")
+    private String nombreCliente; // Nombre del cliente
 }
--- ProyectoFacturacion/src/main/java/com/coderhouse/models/Producto.java
+++ ProyectoFacturacion/src/main/java/com/coderhouse/models/Producto.java
@@ -1,5 +1,6 @@
 package com.coderhouse.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,5 +13,18 @@ public class Producto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // Identificador único del cliente
 
-    private String nombre; // Nombre del cliente
+    @Column(nullable = false) // No puede ser nulo
+    private String nombreProducto;
+    
+    @Column(nullable = false) // No puede ser nulo
+    private double precio;
+    
+    @Column(nullable = false) // No puede ser nulo
+    private int stock;
+   
+    
+    
+    
+    
 }
+
