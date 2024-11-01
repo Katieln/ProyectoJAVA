adf0b355e8201822d90fd3554a3b50bea48b2fa5
Katieln <139283838+Katieln@users.noreply.github.com>
2024-11-01 08:24:56 -0500

    fix

--- ProyectoFacturacion/src/main/java/com/coderhouse/models/Factura.java
+++ ProyectoFacturacion/src/main/java/com/coderhouse/models/Factura.java
@@ -2,12 +2,13 @@ package com.coderhouse.models;
 
 import java.util.Date;
 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 
@@ -27,54 +28,6 @@ public class Factura {
 
     @Column(nullable = false) // Total de la factura (no puede ser nulo)
     private double total;
-    
-    @OneToMany(mappedBy = "factura") // Relación One-to-Many con DetalleFactura
-    private List<DetalleFactura> detalles;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getClienteId() {
-		return clienteId;
-	}
-
-	public void setClienteId(Long clienteId) {
-		this.clienteId = clienteId;
-	}
-
-	public Date getFecha() {
-		return fecha;
-	}
-
-	public void setFecha(Date fecha) {
-		this.fecha = fecha;
-	}
-
-	public double getTotal() {
-		return total;
-	}
-
-	public void setTotal(double total) {
-		this.total = total;
-	}
-
-	public List<DetalleFactura> getDetalles() {
-		return detalles;
-	}
-
-	public void setDetalles(List<DetalleFactura> detalles) {
-		this.detalles = detalles;
-	}
-
-	@Override
-	public String toString() {
-		return "Factura [id=" + id + ", clienteId=" + clienteId + ", fecha=" + fecha + ", total=" + total + "]";
-	}
 
     
     
--- ProyectoFacturacion/src/main/java/com/coderhouse/models/Producto.java
+++ ProyectoFacturacion/src/main/java/com/coderhouse/models/Producto.java
@@ -1,10 +1,13 @@
 package com.coderhouse.models;
 
+
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
 
 @Entity
 public class Producto {
@@ -21,47 +24,8 @@ public class Producto {
     
     @Column(nullable = false) // No puede ser nulo
     private int stock;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNombreProducto() {
-		return nombreProducto;
-	}
-
-	public void setNombreProducto(String nombreProducto) {
-		this.nombreProducto = nombreProducto;
-	}
-
-	public double getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(double precio) {
-		this.precio = precio;
-	}
-
-	public int getStock() {
-		return stock;
-	}
-
-	public void setStock(int stock) {
-		this.stock = stock;
-	}
-
-	@Override
-	public String toString() {
-		return "Producto [id=" + id + ", nombreProducto=" + nombreProducto + ", precio=" + precio + ", stock=" + stock
-				+ "]";
-	}
    
-    
-    
+
     
     
 }
