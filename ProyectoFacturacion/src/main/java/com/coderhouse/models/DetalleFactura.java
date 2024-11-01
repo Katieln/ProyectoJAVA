cd270f19eb223c460737258a572b6d7c4b9fdd78
Katieln <139283838+Katieln@users.noreply.github.com>
2024-11-01 06:56:42 -0500

    alltablesgood

--- ProyectoFacturacion/src/main/java/com/coderhouse/models/DetalleFactura.java
+++ ProyectoFacturacion/src/main/java/com/coderhouse/models/DetalleFactura.java
@@ -1,5 +1,6 @@
 package com.coderhouse.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,5 +13,21 @@ public class DetalleFactura {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // Identificador único del cliente
 
-    private String nombre; // Nombre del cliente
+    @Column(name = "factura_id", nullable = false) // Relación con la factura (no puede ser nulo)
+    private Long facturaId;
+
+    @Column(name = "producto_id", nullable = false) // Relación con el producto (no puede ser nulo)
+    private Long productoId;
+
+    @Column(nullable = false) // Cantidad de productos en la factura (no puede ser nulo)
+    private int cantidad;
+
+    @Column(nullable = false) // Subtotal de la línea (no puede ser nulo)
+    private double subtotal;
+    
+    
+    
+    
+    
+    
 }
--- ProyectoFacturacion/src/main/java/com/coderhouse/models/Factura.java
+++ ProyectoFacturacion/src/main/java/com/coderhouse/models/Factura.java
@@ -1,9 +1,14 @@
 package com.coderhouse.models;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
 public class Factura {
@@ -12,5 +17,14 @@ public class Factura {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // Identificador único del cliente
 
-    private String nombre; // Nombre del cliente
+	@Column(name = "cliente_id", nullable = false) // Relación con el cliente (no puede ser nulo)
+    private Long clienteId;
+
+    @Temporal(TemporalType.DATE) // Tipo de dato para la fecha
+    @Column(nullable = false) // No puede ser nulo
+    private Date fecha;
+
+    @Column(nullable = false) // Total de la factura (no puede ser nulo)
+    private double total;
+
 }
