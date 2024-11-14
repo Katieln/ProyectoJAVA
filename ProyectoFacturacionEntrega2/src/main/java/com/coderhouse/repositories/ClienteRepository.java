598b50ed30ea17916da93006e73a23f11416bdbf
Katieln <139283838+Katieln@users.noreply.github.com>
2024-11-14 02:24:16 -0500

    .

--- ProyectoFacturacionEntrega2/src/main/java/com/coderhouse/repositories/ClienteRepository.java
+++ ProyectoFacturacionEntrega2/src/main/java/com/coderhouse/repositories/ClienteRepository.java
@@ -1,5 +1,11 @@
 package com.coderhouse.repositories;
 
-public interface ClienteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
-}
+import com.coderhouse.models.Cliente;
+
+public interface ClienteRepository extends JpaRepository <Cliente, Long> {}
+
+
+
+
