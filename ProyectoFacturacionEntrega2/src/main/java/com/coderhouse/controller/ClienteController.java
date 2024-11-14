1663f8854c972a9eaeef89603686572928e32e3d
Katieln <139283838+Katieln@users.noreply.github.com>
2024-11-14 04:14:00 -0500

    cl

--- ProyectoFacturacionEntrega2/src/main/java/com/coderhouse/controller/ClienteController.java
+++ ProyectoFacturacionEntrega2/src/main/java/com/coderhouse/controller/ClienteController.java
@@ -1,5 +1,105 @@
 package com.coderhouse.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coderhouse.models.Cliente;
+import com.coderhouse.services.ClienteService;
+
+@RestController
+@RequestMapping("/api/cliente")
 public class ClienteController {
+
+	@Autowired
+	private ClienteService ClienteService;
+
+	//*********GetAllClientes***********//
+	@GetMapping
+	public ResponseEntity <List<Cliente>> getAllClientes(){
+		try {
+			List<Cliente> Clientes = ClienteService.getAllClients();
+			return ResponseEntity.ok(Clientes);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+
+
+	//*********GetClienteByID***********//
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> getAlumnoById(@PathVariable long id) {
+		try {
+			Cliente Cliente = ClienteService.findById(id);
+			return ResponseEntity.ok(Cliente);
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.notFound().build();
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+
+	//**********PostNewCliente**********//
+	@PostMapping
+	public ResponseEntity<Cliente> createCliente(@RequestBody Cliente Cliente) {
+		try {
+			Cliente newCliente = ClienteService.saveCliente(Cliente);
+			return ResponseEntity.ok(newCliente);
+
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+
+
+	//**********AddMultipleClientes**********//
+	@PostMapping("/list")
+	public ResponseEntity<List<Cliente>> saveClientes(@RequestBody List<Cliente> Clientes) {
+	    List<Cliente> savedClientes = ClienteService.saveCliente(Clientes);
+	    return new ResponseEntity<>(savedClientes, HttpStatus.CREATED);
+	}
+
+
+	//*********DeleteClienteByID**********//
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteCliente(@PathVariable Long id) {
+		try {
+			ClienteService.deleteCliente(id);
+			return ResponseEntity.noContent().build();
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.notFound().build();
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+
+
+
+	//*********UpdateClienteByID**********//
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> updateClienteById(@PathVariable Long id, @RequestBody Cliente ClienteDetails) {
+		try {
+			Cliente updatedCliente = ClienteService.updateCliente(id, ClienteDetails);
+			return ResponseEntity.ok(updatedCliente);
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.notFound().build();
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+
+
+
+
 
 }
--- ProyectoFacturacionEntrega2/src/main/java/com/coderhouse/services/ClienteService.java
+++ ProyectoFacturacionEntrega2/src/main/java/com/coderhouse/services/ClienteService.java
@@ -1,5 +1,65 @@
 package com.coderhouse.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.coderhouse.models.Cliente;
+import com.coderhouse.repositories.ClienteRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
 public class ClienteService {
+
+	@Autowired
+	private ClienteRepository ClienteRepository;
+
+//*********GetAllClientes***********//
+	public List<Cliente> getAllClients() {
+		return ClienteRepository.findAll();
+	}
+
+
+//*********GetClienteByID************//
+	public Cliente findById(Long id) {
+		return ClienteRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Cliente no encontrado"));
+	}
+
+//**********AddNewCliente**********//
+	@Transactional
+	public Cliente saveCliente(Cliente Cliente) {
+		return ClienteRepository.save(Cliente);
+	}
+
+
+//**********AddMultipleClientes**********//
+	@Transactional
+	public List<Cliente> saveCliente(List<Cliente> Clientes) {
+	    return ClienteRepository.saveAll(Clientes);
+	}
+
+
+//*********DeleteClienteByID**********//
+	public void deleteCliente(Long id) {
+		if (!ClienteRepository.existsById(id)) {
+			throw new IllegalArgumentException("Cliente no encontrado");
+		}
+		ClienteRepository.deleteById(id);
+	}
+
+//*********UpdateClienteByID**********//
+	@Transactional
+	public Cliente updateCliente(Long id, Cliente ClienteDetails) {
+		Cliente Cliente = ClienteRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Alumno no encontrado"));
+
+		Cliente.setNombreCliente(ClienteDetails.getNombreCliente());
+
+
+
+		return ClienteRepository.save(Cliente);
+	}
 
 }
